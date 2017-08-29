e0841f409160414fc8e4f4cd230d0e021b486505
arvo79 <eichner.f@googlemail.com>
2017-08-29 08:33:40 +0200

    Implemented custom action for updating statuses for blog posts

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,12 @@ Rails.application.routes.draw do
 	
   root 'portfolios#index'
 
-  resources :blogs
+  resources :blogs do
+  	member do
+  		get :toggle_status
+  	end
+  end
+
   resources :portfolios, except: [:show]
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
 
