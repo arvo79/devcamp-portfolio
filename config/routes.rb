2e250ed31afd98a2632eb8bd77a65d39707bff5b
arvo79 <eichner.f@googlemail.com>
2017-09-01 10:05:26 +0200

    customized registration, login and logout routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 	
-  devise_for :users
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
+
   resources :portfolios, except: [:show]
   get 'no1', to: 'portfolios#no1'
   get 'no9', to: 'portfolios#no9'
