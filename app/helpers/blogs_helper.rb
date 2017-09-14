074ab799010d443e8e08b42d924674ef5da30e68
arvo79 <eichner.f@googlemail.com>
2017-09-14 19:44:08 +0200

    styled blog status indication

--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -3,6 +3,12 @@ module BlogsHelper
 		image_tag "https://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(user.email)}", width: 60
 	end
 
+	def status_color_helper blog
+		if blog.draft?
+			"color: orange;"
+		end
+	end
+
 	class CodeRayify < Redcarpet::Render::HTML
 		def block_code(code, language)
 			CodeRay.scan(code, language).div
