a93da7c841b1d8932831c0f53ccb220bb565899f
arvo79 <eichner.f@googlemail.com>
2017-09-15 16:18:17 +0200

    added css animated text on home page, made final styling changes to nav bars and fixed action cable bug

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_cable.allowed_request_origins = ['https://portfolio-homepage.herokuapp.com', 'http://portfolio-homepage.herokuapp.com']
+  config.action_cable.url = "wss://portfolio-homepage.herokuapp.com/cable"
 end
